48c56ec809ac3f1d138cb6614b334719cc26e336
thoriq-hs <86838534+thoriq-hs@users.noreply.github.com>
2021-08-06 15:56:40 +0700

    Add files via upload
    Menambahkan file if-else.py dan elif.py

---
+++ elif.py
@@ -0,0 +1,15 @@
+# Belajar elif
+
+menu_pilihan =input("silahkan pilih menu [1-3] :")
+
+if menu_pilihan == "1":
+    print("bakso")
+elif menu_pilihan == "2":
+    print("mie ayam")
+elif menu_pilihan == "3":
+    print("roti")
+else :
+    print("Menu tidak tersedia")
+
+if menu_pilihan == "x":
+    print("program keluar")
---
+++ if-else.py
@@ -0,0 +1,9 @@
+# Belajar if-else
+
+menang = True
+
+if menang:
+    print("Selamat bro")
+
+else:
+    print("Anda kurang berang beruntung")
